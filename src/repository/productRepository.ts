3afe52a57e8e4cb3cfa13a3f73ba42b759878fce
Pavel Zubarevich <pavel.zubarevich@itechart-group.com>
2022-04-01 15:25:38 +0300

    add filtering

--- src/repository/productRepository.ts
+++ src/repository/productRepository.ts
@@ -1,14 +1,36 @@
 import { Response, Request, NextFunction } from 'express';
 import { MongoProduct } from '../models';
 import { SQLProduct } from '../entity';
-import { IProductRepository } from '../types/types';
+import { IProductRepository, IFindProps, ITotalRatingFilter, ISortProps } from '../types/types';
 import { AppDataSource } from '../db/postgresql';
 
 const mongo = 'mongo';
 
 class ProductTypegooseRepository implements IProductRepository {
   async all(req: Request, res: Response, next: NextFunction) {
-    const products = await MongoProduct.find();
+    const query = req.query;
+    const displayNameReg: RegExp = new RegExp(`${query.displayName}`);
+    const prices: Array<string> = query.price?.toString().split(':') || [];
+    const findProps: IFindProps = {};
+
+    query.displayName && (findProps.displayName = displayNameReg);
+    query.minRating && (findProps.totalRating = { $gt: query.minRating });
+
+    if (prices[0] || prices[1]) {
+      const totalRatingFilter: ITotalRatingFilter = {};
+      +prices[0] && (totalRatingFilter.$gt = +prices[0]);
+      +prices[1] && (totalRatingFilter.$lt = +prices[1]);
+      findProps.price = totalRatingFilter;
+    }
+
+    const sortProps: ISortProps = {};
+    const sortingParams: Array<string> = query.sortBy?.toString().split(':') || [];
+
+    if (sortingParams[0] && sortingParams[1]) {
+      sortProps[sortingParams[0]] = sortingParams[1];
+    }
+
+    const products = await MongoProduct.find(findProps).sort(sortProps).exec();
 
     if (products) {
       res.send(products);
