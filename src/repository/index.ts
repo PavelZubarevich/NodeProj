34c8b540ede3a3c9ccd348e4c76946c8121e8457
Pavel Zubarevich <pavel.zubarevich@itechart-group.com>
2022-05-03 23:26:52 +0300

    refactor with graphQl

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -3,5 +3,13 @@ import CategoryRepository from './catalogRepository';
 import UserRepository from './userRepository';
 import OrderListRepository from './orderListRepository';
 import OrderProductRepository from './orderProductRepository';
+import SessionRepository from './sessionRepository';
 
-export { ProductRepository, CategoryRepository, UserRepository, OrderListRepository, OrderProductRepository };
+export {
+  ProductRepository,
+  CategoryRepository,
+  UserRepository,
+  OrderListRepository,
+  OrderProductRepository,
+  SessionRepository
+};
