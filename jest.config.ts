bcc98f15b0d63a920645cf67dd7231275e938161
Pavel Zubarevich <pavel.zubarevich@itechart-group.com>
2022-05-11 14:21:47 +0300

    add jest config

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src', '<rootDir>'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  clearMocks: true
+};
