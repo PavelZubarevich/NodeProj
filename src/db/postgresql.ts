b82ec935a58cb14dd122bc440771cc6a1d1557f3
Pavel Zubarevich <pavel.zubarevich@itechart-group.com>
2022-05-13 10:40:13 +0300

    fix

--- src/db/postgresql.ts
+++ src/db/postgresql.ts
@@ -13,7 +13,7 @@ import {
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
+  // ssl: { rejectUnauthorized: false },
   url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
